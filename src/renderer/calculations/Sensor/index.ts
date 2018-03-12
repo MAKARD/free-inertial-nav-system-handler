6050c0e5f7947d9a66c24241b6f4be77e5b869ba
MAKARD <human.getEmail@gmail.com>
2018-03-12 22:09:19 +0200

    Implement Sensor class and helper classes for it

---
+++ src/renderer/calculations/Sensor/index.ts
@@ -0,0 +1,2 @@
+export * from "./Sensor";
+export * from "./SensorAxis";
---
+++ src/renderer/calculations/TypedClass.ts
@@ -0,0 +1,21 @@
+import * as PropTypes from "prop-types";
+
+interface Types {
+    [key: string]: PropTypes.Validator<any>;
+}
+
+interface Props {
+    [key: string]: any;
+}
+
+export class TypedClass {
+    constructor(props: Props, propTypes: Types) {
+        this.checkTypes(props, propTypes);
+    }
+
+    public checkTypes = (props: Props, propTypes: Types): void | never => {
+        (PropTypes as any).checkPropTypes(propTypes, props, "", "TypedClassChild", () => {
+            throw new Error("Invalid passed props");
+        });
+    }
+}
